2d42e744d577c0eb7b011453ee8ff4f53f8926b4
Jon <XPS-WSL>
2023-09-11 14:55:58 +0200

    update code

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,30 @@
 from pymongo import MongoClient
 
 
-def get_database():
-    CONNECTION_STRING = "mongodb://localhost:27017"
-    client = MongoClient(CONNECTION_STRING)
-
-    return client['coockbookdb']
-
-
-
 if __name__ == "__main__":   
   
-    client = MongoClient()
-    db=client.test
-    storage = db.storage
+    client = MongoClient("mongodb://localhost:27017")
+    client.server_info()
+
+    db = client["mydatabase"]
+    print(db)
+
+    print("5")
+    mycol = db["customers"]
+
+    print("4")
+    mydict = { "name": "John", "address": "Highway 37" }
+    print("6")
+    x = mycol.insert_one(mydict)
+    print("7")
+    print(x.inserted_id)
+
+    print("1")
+    print(client.list_database_names())
+
+    print("2")
+    dblist = client.list_database_names()
+
+    print("3")
+    if "mydatabase" in dblist:
+        print("The database exists.")
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt install python3 python3-pip docker docker-compose -y
 
 pip3 install -r requirements.txt
+
